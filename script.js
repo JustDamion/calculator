39315a63cee45215c3a4ff3caab93c418c39c635
Damion Shirkey <shirkeydamion@gmail.com>
2026-06-29 15:42:53 -0500

    Create function to detect operation

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+    }
+}
