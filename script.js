4a3479aa5171b832ff52fecc761af5ccd499cd12
Damion Shirkey <shirkeydamion@gmail.com>
2026-07-02 11:52:10 -0500

    Add basic DOM updating functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const OPERATORS = ["+", "-", "x", "/", "%"];
+
 function add(a, b) {
     return a + b;
 }
@@ -26,7 +28,7 @@ function operate(num1, num2, operator) {
         case "-":
             subtract(num1, num2);
             break;
-        case "*":
+        case "x":
             multiply(num1, num2);
             break;
         case "/":
@@ -37,3 +39,46 @@ function operate(num1, num2, operator) {
             break;
     }
 }
+
+function updateInput(value) {
+    if (+value || +value === 0 || value === ".") {
+        currentInput.textContent += value;
+    }
+}
+
+function clear() {
+    num1 = null;
+    num2 = null;
+    currentInput.textContent = '';
+    history.textContent = '';
+}
+
+function calculate(operator) {
+    if (num1 === null) {
+        num1 = +currentInput.textContent;
+    } else {
+        num2 = +currentInput.textContent;
+        let num1 = operate(num1, num2, operator);
+    }
+
+    history.textContent += ` ${currentInput.textContent} ${operator} `;
+    currentInput.textContent = '';
+}
+
+const currentInput = document.querySelector(".current-input");
+const history = document.querySelector(".history");
+const inputsContainer = document.querySelector(".inputs");
+
+let num1 = null;
+let num2 = null;
+
+inputsContainer.addEventListener("click", (event) => {
+    const inputValue = event.target.textContent;
+    updateInput(inputValue);
+
+    if (inputValue === "Clear") {
+        clear();
+    } else if (OPERATORS.includes(inputValue)) {
+        calculate(inputValue);
+    }
+});
