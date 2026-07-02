727ac992e7117871093f2c6e8ba5cb0aa2e013eb
Damion Shirkey <shirkeydamion@gmail.com>
2026-07-02 14:51:11 -0500

    Reorganize JS code

--- script.js
+++ script.js
@@ -1,5 +1,74 @@
 const OPERATORS = ["+", "-", "x", "/", "%"];
 
+const currentInput = document.getElementById("currentInput");
+const inputContainer = document.getElementById("inputContainer");
+const errorMessage = document.getElementById("errorMessage");
+
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+let lastInput = null;
+let inputValue = null;
+
+inputContainer.addEventListener("click", (event) => {
+    if (validateInput(event.target.textContent) === 0) {
+        return 0;
+    }
+    inputValue = event.target.textContent;
+    updateInput(inputValue);
+
+    if (inputValue === "Clear") {
+        clear();
+    } else if (inputValue === "=") {
+        secondNumber = +currentInput.textContent;
+        firstNumber = operate(firstNumber, secondNumber, operator)
+        currentInput.textContent = firstNumber;
+        secondNumber = null;
+    }
+
+    lastInput = inputValue;
+});
+
+function updateInput(value) {
+    if (+value || +value === 0) {
+        if (OPERATORS.includes(lastInput)) {
+            currentInput.textContent = '';
+        }
+        currentInput.textContent += value;
+    } else if (OPERATORS.includes(value)) {
+        if (firstNumber === null) {
+            firstNumber = +currentInput.textContent;
+            operator = value;
+            currentInput.textContent = '';
+        } else {
+            secondNumber = +currentInput.textContent;
+            firstNumber = operate(firstNumber, secondNumber, operator)
+            currentInput.textContent = firstNumber;
+            secondNumber = null;
+            operator = value;
+        }
+    }
+}
+
+function validateInput(input) {
+    errorMessage.textContent = '';
+    if (OPERATORS.includes(inputValue) && OPERATORS.includes(input)) {
+        errorMessage.textContent = "Please enter a number";
+        return 0;
+    }
+    if (currentInput.textContent === '' && (OPERATORS.includes(input) || input === "=")) {
+        errorMessage.textContent = "Please enter a number";
+        return 0;
+    }
+}
+
+function clear() {
+    firstNumber = null;
+    secondNumber = null;
+    currentInput.textContent = '';
+    errorMessage.textContent = '';
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -20,91 +89,22 @@ function modulo(a, b) {
     return a % b;
 }
 
-function operate(num1, num2, operator) {
+function operate(a, b, operator) {
     switch (operator) {
         case "+":
-            return add(num1, num2);
+            return add(a, b);
             break;
         case "-":
-            return subtract(num1, num2);
+            return subtract(a, b);
             break;
         case "x":
-            return multiply(num1, num2);
+            return multiply(a, b);
             break;
         case "/":
-            return divide(num1, num2);
+            return divide(a, b);
             break;
         case "%":
-            return modulo(num1, num2);
+            return modulo(a, b);
             break;
     }
 }
-
-function updateInput(value) {
-    if (+value || +value === 0) {
-        if (OPERATORS.includes(lastInput)) {
-            currentInput.textContent = '';
-        }
-        currentInput.textContent += value;
-    } else if (OPERATORS.includes(value)) {
-        if (num1 === null) {
-            num1 = +currentInput.textContent;
-            operator = value;
-            currentInput.textContent = '';
-        } else {
-            num2 = +currentInput.textContent;
-            num1 = operate(num1, num2, operator)
-            currentInput.textContent = num1;
-            num2 = null;
-            operator = value;
-        }
-    }
-}
-
-function validateInput(input) {
-    error.textContent = '';
-    if (OPERATORS.includes(inputValue) && OPERATORS.includes(input)) {
-        error.textContent = "Please enter a number";
-        return 0;
-    }
-    if (currentInput.textContent === '' && (OPERATORS.includes(input) || input === "=")) {
-        error.textContent = "Please enter a number";
-        return 0;
-    }
-}
-
-function clear() {
-    num1 = null;
-    num2 = null;
-    currentInput.textContent = '';
-    error.textContent = '';
-}
-
-const currentInput = document.querySelector(".current-input");
-const inputsContainer = document.querySelector(".inputs");
-const error = document.querySelector(".error");
-
-let num1 = null;
-let num2 = null;
-let operator = null;
-let lastInput = null;
-let inputValue = null;
-
-inputsContainer.addEventListener("click", (event) => {
-    if (validateInput(event.target.textContent) === 0) {
-        return 0;
-    }
-    inputValue = event.target.textContent;
-    updateInput(inputValue);
-
-    if (inputValue === "Clear") {
-        clear();
-    } else if (inputValue === "=") {
-        num2 = +currentInput.textContent;
-        num1 = operate(num1, num2, operator)
-        currentInput.textContent = num1;
-        num2 = null;
-    }
-
-    lastInput = inputValue;
-});
